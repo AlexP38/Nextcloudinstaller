b06fe145c4b55ebec49b18813f457a8c701ea32f
Alex Paltian <alex@paltian.net>
2022-02-19 17:55:59 +0100

    Restart Apache after deleting

--- DeleteContainer.sh
+++ DeleteContainer.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 read -p 'Container-Name: ' container
 a2dissite 000-nextcloud-container.conf
+systemctl restart apache2
 rm /etc/apache2/sites-available/000-nextcloud-container.conf
 lxc stop $container && lxc delete $container
