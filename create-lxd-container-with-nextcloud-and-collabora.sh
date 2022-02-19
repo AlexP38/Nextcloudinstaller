8dd429422ca426badb2c9b877609fccb100d7fa9
Alex Paltian <alex@paltian.net>
2022-02-19 14:19:59 +0100

    push files in container

--- create-lxd-container-with-nextcloud-and-collabora.sh
+++ create-lxd-container-with-nextcloud-and-collabora.sh
@@ -7,9 +7,10 @@ read -p 'Now what is the ending? e.g. de / com / net (without a .):' tdl
 read -p 'Where is your reverse-proxy? (You need an apache2 and certbot installed) Tell me the container name or if on the host say "host":' revproxy
 lxc init images:debian/11 $container
 lxc start $container
-lxc exec $container cd /usr/share/keyrings
-lxc exec $container wget https://collaboraoffice.com/downloads/gpg/collaboraonline-release-keyring.gpg
-lxc exec $container cp collaboraonline.sources > /etc/apt/sources.list.d/collaboraonline.sources
+lxc exec $container wget https://collaboraoffice.com/downloads/gpg/collaboraonline-release-keyring.gpg -P /usr/share/keyrings
+lxc file push vhost.conf $container/root/
+lxc file push collabora.sources $container/root/
+lxc exec $container cp /root/collaboraonline.sources > /etc/apt/sources.list.d/collaboraonline.sources
 lxc exec $container apt update && apt -y install mariadb-server mariadb-client nano wget curl unzip php php-{cli,xml,zip,curl,gd,cgi,mysql,mbstring} apache2 libapache2-mod-php coolwsd code-brand
 lxc exec $container mysql --user="root" --execute="CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY '$password'; CREATE DATABASE nextcloud; GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost'; FLUSH PRIVILEGES;"
 lxc exec $container a2enmod headers
@@ -23,7 +24,7 @@ lxc exec $container unzip nextcloud-23.zip
 lxc exec $container mv nextcloud /var/www/
 lxc exec $container chown -R www-data:www-data /var/www/nextcloud && chmod -R 755 /var/www/nextcloud
 lxc exec $container rm -r nextcloud-23.zip
-lxc exec $container cp vhost.conf > /etc/apache2/sites-available/000-nextcloud.conf
+lxc exec $container cp /root/vhost.conf > /etc/apache2/sites-available/000-nextcloud.conf
 lxc exec $container sed -i -r 's/replacewithdomain/$domain/g' /etc/apache2/sites-available/000-nextcloud.conf
 lxc exec $container a2ensite 000-nextcloud.conf
 lxc exec $container systemctl restart apache2
@@ -33,17 +34,19 @@ lxc exec $container sed '/<wopi desc="Allow/deny wopi storage." allow="true">/ a
 lxc exec $container systemctl restart coolwsd
 if [ $revproxy != 'host' ]
 then
-lxc exec $revproxy cp vhost-reverse-proxy.conf > /etc/apache2/sites-available/000-nextcloud-container.conf
+lxc file push vhost-reverse-proxy.conf $revproxy/root/
+lxc exec $revproxy cp /root/vhost-reverse-proxy.conf > /etc/apache2/sites-available/000-nextcloud-container.conf
 lxc exec $revproxy sed -i -r 's/replacewithdomain/$domain/g' /etc/apache2/sites-available/000-nextcloud-container.conf
 lxc exec $revproxy sed -i -r 's/replacewithcontainer/$container/g' /etc/apache2/sites-available/000-nextcloud-container.conf
 lxc exec $revproxy certbot certonly -d $domain --apache
 lxc exec $revproxy a2ensite 000-nextcloud-container.conf
 lxc exec $revproxy systemctl reload apache2
-fi
-cp vhost-reverse-proxy.conf > /etc/apache2/sites-available/000-nextcloud-container.conf
+else
+ cp vhost-reverse-proxy.conf > /etc/apache2/sites-available/000-nextcloud-container.conf
 sed -i -r 's/replacewithdomain/$domain/g' /etc/apache2/sites-available/000-nextcloud-container.conf
 sed -i -r 's/replacewithcontainer/$container/g' /etc/apache2/sites-available/000-nextcloud-container.conf
 certbot certonly -d $domain --apache
 a2ensite 000-nextcloud-container.conf
 systemctl reload apache2
+fi
 echo 'Done. Got to https://$domain and set up your nextcloud with a admin user and the database credentials user: nextcloud, password: $password and database-name: nextcloud.'
