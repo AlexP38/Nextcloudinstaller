b6b8ee04840f7d3fd61c448ffc634771df7ff6fd
Alex Paltian <alex@paltian.net>
2022-02-20 16:43:49 +0100

    Started Script for creating Reverse Proxy and other rev. Proxy fixes in create script

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ sh DeleteContainer.sh    (to delete a Nextcloud)
 
 After this script it is ready to use. No more action is required.
 
-You need a Linux-Server with lxd installed and either a lxd container with running apache and certbot as reverse proxy (also dont forget to forward http and https ports to this reverse-Proxy) or apache and certbot running on the host as proxy. The script will insert automatically a vhost to the apache to activate the proxy and request a certificate for https.
+You need a Linux-Server with lxd installed and either a lxd container with running apache and certbot as reverse proxy (also dont forget to forward http and https ports to this reverse-Proxy) or apache and certbot and python3-certbot-apache running on the host as proxy. The script will insert automatically a vhost to the apache to activate the proxy and request a certificate for https.
--- create-lxd-container-with-nextcloud-and-collabora.sh
+++ create-lxd-container-with-nextcloud-and-collabora.sh
@@ -80,8 +80,8 @@ then
 lxc file push vhost-reverse-proxy.conf $revproxy/root/vhost-reverse-proxy.conf
 lxc exec $revproxy -- sh -c 'mv /root/vhost-reverse-proxy.conf /etc/apache2/sites-available/'"$container"'-'"$container2"'-container.conf && \
 sed -i -r "s/replacewithdomain/'"$domain"'/g" /etc/apache2/sites-available/'"$container"'-'"$container2"'-container.conf && \
-sed -i -r "s/replacewithcontainer/'"$container"'/g" /etc/apache2/sites-available/'"$container"'-'"$container2"'-container.conf && \
-sed -i -r "s/replacewithcollaboracontainer/'"$container2"'/g" /etc/apache2/sites-available/'"$container"'-'"$container2"'-container.conf && \
+sed -i -r "s/replacewithcontainer/'"$ContainerIP"'/g" /etc/apache2/sites-available/'"$container"'-'"$container2"'-container.conf && \
+sed -i -r "s/replacewithcollaboracontainer/'"$ContainerIP2"'/g" /etc/apache2/sites-available/'"$container"'-'"$container2"'-container.conf && \
 certbot certonly -d $domain --apache && \
 a2ensite '"$container"'-'"$container2"'-container.conf && \
 systemctl reload apache2'
